bc64125439c1dbdada3205774c615bc006998441
ali-alhaddad <alialhaddad708@gmail.com>
2022-03-20 18:26:20 +0300

    ali

--- BMI Calculator/script.js
+++ BMI Calculator/script.js
@@ -1,1 +1,23 @@
-// هنا سوف تنشئ دالة calculate 
+// هنا سوف تنشئ دالة calculate
+function calculate() {
+  let height = document.getElementById("height").value;
+  let weight = document.getElementById("weight").value;
+  if (height == "" || weight == "") {
+    alert(" قم بالتأكد من تعبئة جميع بياناتك المطلوبة");
+    return;
+  }
+  let BML = weight / (height * height);
+  let status = "";
+  if (BML < 18.5) {
+    status = "تعاني من نقص في الوزن";
+  } else if (BML < 25) {
+    status = "وزنك صحي";
+  } else if (BML < 30) {
+    status = "تعاني من زيادة في الوزن";
+  } else {
+    status = "تعاني من السمنة المفرطة";
+  }
+  BML = BML.toFixed(1);
+  document.getElementById("result").innerHTML = BML;
+  document.getElementsByClassName("comment")[0].innerHTML = status;
+}
---
+++ Exercise 1/ex1.js
@@ -0,0 +1,5 @@
+(function travel(x = "زمردة") {
+  console.log(" اود السفر الى كوكب" + x);
+})("زحل");
+let planet = prompt("اذكر اسم الكوكب الذي تود الذهاب اليه");
+travel(planet);
